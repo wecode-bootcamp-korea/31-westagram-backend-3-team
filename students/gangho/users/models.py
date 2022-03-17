e646cc801e8c800cb09a30bb43bbfd23e99f5241
gleehave <gleehave@gmail.com>
2022-03-17 20:42:22 +0900

    [이강호][add] users의 모델 작성(makemigrations-migrate 수행 전)

--- students/gangho/users/models.py
+++ students/gangho/users/models.py
@@ -1,3 +1,39 @@
+import re
+
+from django.core.exceptions import ValidationError
 from django.db import models
 
-# Create your models here.
+# models.py에 사용자를 관리할 클래스를 생성합니다.
+# 생성된 테이블을 사용해 회원가입과 로그인 기능을 작성하게 됩니다.
+# 회원가입을 할 때에는 아래와 같은 사용자의 정보를 입력해야 합니다.
+
+# 이름
+# 이메일
+# 비밀번호
+# 연락처(휴대폰)
+
+def validate_email(email):
+    # 이메일 유효성 검사에 뭐가 필요하지?
+    # e.g.) gleehave@gmail.com
+
+    # 1. id는 특수문자 없어야 함.
+    # 2. @는 꼭 필요함
+    # 3. 뒤에는 aaa@gmail.com -> .com
+    email_expression = re.compile('^[a-zA-Z0-9+-_.]]+@[a-zA-Z]+\.[a-z]]+$')
+    if not email_expression.match(email):
+        raise ValidationError(
+            ('%(value)s is not an email expression'),
+            params={'email': email},
+        )
+
+class User(models.Model):
+    name        = models.CharField(max_length=45, db_column='name')
+    email       = models.EmailField(validators=[validate_email], db_column='email')
+    password    = models.CharField(max_length=30, db_column='password')
+    cell_phone  = models.CharField(max_length=30, db_column='cell_phone')
+
+    class Meta:
+        db_table = 'users'
+
+    def __str__(self):
+        return self.name
