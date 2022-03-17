02eba73aaea230fad656209d9e303550f0089b72
leesan195159 <sniping1997@naver.com>
2022-03-17 21:24:43 +0900

    중간저장

--- students/leesan/users/models.py
+++ students/leesan/users/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name         = models.CharField(max_length=50)
     email        = models.EmailField(max_length=50, unique=True)
     password     = models.CharField(max_length=50)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=50)
 
     class Meta:
         db_table = 'users'
--- students/leesan/users/views.py
+++ students/leesan/users/views.py
@@ -8,26 +8,31 @@ import re
 class UserView(View):
     def post(self, request):
 
-            data                = json.loads(request.body)
-            name                = data['name']
-            email               = data['email']
-            email_validation    = re.compile('^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-            password            = data['password']
-            password_validation = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$")
-            phone_number        = data['phone_number']
-
+            data                    = json.loads(request.body)
+            name                    = data['name']
+            email                   = data['email']
+            email_validation        = re.compile('^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+            password                = data['password']
+            password_validation     = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$")
+            phone_number            = data['phone_number']
+            
+            
+            
             if email == "" or password == "":
-                return JsonResponse({"mesage": "Email과 Password를 입력하세요"}, status=400)
+                return JsonResponse({"message": "Email과 Password를 입력하세요"}, status=400)
             if not email_validation.match(email):
-                return JsonResponse({"mesage": "Email을 형식에 맞게 입력하세요"}, status=400)
+                return JsonResponse({"message": "Email을 형식에 맞게 입력하세요"}, status=400)
             if not password_validation.match(password):
-                return JsonResponse({"mesage": "Password를 형식에 맞게 입력하세요"}, status=400)
-
-            User.objects.create(
-                name         = name,
-                email        = email,
-                password     = password,
-                phone_number = phone_number
-                )
-            return JsonResponse({"message" : "SUCCESS"}, status=201)
+                return JsonResponse({"message": "Password를 형식에 맞게 입력하세요"}, status=400)
+                
+            result = User.objects.filter(email=email)
+            if not result:
+                User.objects.create(
+                    name         = name,
+                    email        = email,
+                    password     = password,
+                    phone_number = phone_number,
+                    )
+                return JsonResponse({"message" : "SUCCESS"}, status=201)
+            return JsonResponse({"message": "다른 사용자가 사용하는 email입니다. 다시 입력해주세요."}, status=400)
 
