059164a40cccf1a315d846eeb5f4e461f67302e4
gleehave <gleehave@gmail.com>
2022-03-17 21:32:36 +0900

    [이강호][add] cell phone의 validate 메소드 작성

--- students/gangho/users/models.py
+++ students/gangho/users/models.py
@@ -1,23 +1,15 @@
 import re
 
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from django.db import models
-
-# models.py에 사용자를 관리할 클래스를 생성합니다.
-# 생성된 테이블을 사용해 회원가입과 로그인 기능을 작성하게 됩니다.
-# 회원가입을 할 때에는 아래와 같은 사용자의 정보를 입력해야 합니다.
-
-# 이름
-# 이메일
-# 비밀번호
-# 연락처(휴대폰)
 
 def validate_email(email):
     email_expression = re.compile('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')
 
     if not email_expression.match(email):
         raise ValidationError(
-            '%(value)s is not an email expression',
+            _('%(value)s is not an email expression'),
             params={'email': email},
         )
 
@@ -26,15 +18,24 @@ def validate_password(password):
 
     if not password_expression.match(password):
         raise ValidationError(
-            ('%(value)s is not an password expression'),
+            _('%(value)s is not an password expression'),
             params={'password': password},
+        )
+
+def validate_cell_phone(cell_phone):
+    cell_phone_expression = re.compile('[]]')
+
+    if not cell_phone_expression.match(cell_phone):
+        raise ValidationError(
+            _('%(value)s is not an email expression'),
+            params={'email': cell_phone},
         )
 
 class User(models.Model):
     name        = models.CharField(max_length=45, db_column='name')
     email       = models.EmailField(validators=[validate_email], db_column='email')
-    password    = models.CharField(validators=[validate_password],max_length=30, db_column='password')
-    cell_phone  = models.CharField(max_length=30, db_column='cell_phone')
+    password    = models.CharField(validators=[validate_password], max_length=30, db_column='password')
+    cell_phone  = models.CharField(validators=[validate_cell_phone], max_length=30, db_column='cell_phone')
 
     class Meta:
         db_table = 'users'
