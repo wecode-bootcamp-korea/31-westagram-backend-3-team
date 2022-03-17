686e442bff7c47eb79ad696821b32955b59f67dc
leesan195159 <sniping1997@naver.com>
2022-03-17 16:50:58 +0900

    Create: User 테이블 생성

--- students/leesan/users/models.py
+++ students/leesan/users/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField()
+    name         = models.CharField(max_length=50)
+    email        = models.EmailField(max_length=50)
+    password     = models.CharField()
     phone_number = models.IntegerField()
-    address = models.CharField(max_length=50)
+    address      = models.CharField(max_length=50)
 
     class Meta:
         db_table = 'users'
