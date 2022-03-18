f567e3ff54f10c4575760ca94f2fb86199c66a74
gleehave <gleehave@gmail.com>
2022-03-18 16:10:09 +0900

    [이강호][refactor] email에 max_length 추가

--- students/gangho/users/models.py
+++ students/gangho/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     name          = models.CharField(max_length=45)
-    email         = models.EmailField(unique=True)
+    email         = models.EmailField(max_length=100, unique=True)
     password      = models.CharField(max_length=30)
     phone_number  = models.CharField(max_length=30)
     created_at    = models.DateTimeField(auto_now_add=True)
