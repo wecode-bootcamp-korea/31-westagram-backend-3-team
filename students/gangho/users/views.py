f6a47c7428ce32701d9ced0029a7bb5ccb674c59
gleehave <gleehave@gmail.com>
2022-03-18 14:19:02 +0900

    [이강호][add] UserCreateView 작성

--- students/gangho/users/views.py
+++ students/gangho/users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
 
-# Create your views here.
+from users.models import User
+
+
+class UserCreateView(View):
+    def post(self, request):
+        data = request.body
+
+        if data['email']:
+            comparison_data = User.objects.get(email=data['email'])
+            if comparison_data is None:
+                User.objects.create(
+                    name        = data["name"],
+                    email       = data["email"],
+                    password    = data["password"],
+                    cell_phone  = data['cell_phone']
+                )
+            else:
+                return JsonResponse({'error': '이미 회원가입된 이메일 입니다.'}, status=401)
+
+        return JsonResponse({'message': 'SUCCESS'}, status=201)
