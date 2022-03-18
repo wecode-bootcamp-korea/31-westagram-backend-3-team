4c93054686030acefafb7786bc692a275718ec26
gleehave <gleehave@gmail.com>
2022-03-18 16:55:27 +0900

    [이강호][refacgor] password 최대 길이 100으로 변경

--- students/gangho/users/models.py
+++ students/gangho/users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
     name          = models.CharField(max_length=45)
     email         = models.EmailField(max_length=100, unique=True)
-    password      = models.CharField(max_length=30)
-    phone_number  = models.CharField(max_length=30)
+    password      = models.CharField(max_length=100)
+    phone_number  = models.CharField(max_length=45)
     created_at    = models.DateTimeField(auto_now_add=True)
     updated_at    = models.DateTimeField(auto_now=True)
 
