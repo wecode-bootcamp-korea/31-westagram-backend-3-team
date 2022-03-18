0fb9c85330c4cf92b3a674dfc39e2dd0d6cb7b7e
leesan195159 <sniping1997@naver.com>
2022-03-18 16:39:47 +0900

    modified: 예외처리 수정

--- students/leesan/users/urls.py
+++ students/leesan/users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/signin', SignIn.as_view())
+    path('/signup', SignUpView.as_view())
 ]
--- students/leesan/users/views.py
+++ students/leesan/users/views.py
@@ -6,35 +6,37 @@ from django.http import JsonResponse
 
 from .models import User
 
-class SignIn(View):
+class SignUpView(View):
     def post(self, request):
-        data                    = json.loads(request.body)
-        name                    = data['name']
-        email                   = data['email']
-        email_validation        = re.compile('^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
-        password                = data['password']
-        password_validation     = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$")
-        phone_number            = data['phone_number']
-        phone_number_validation = re.compile("^\d{3}-\d{3,4}-\d{4}$")
-        address                 = data['address']
-        result                  = User.objects.filter(email=email)
+        try:
+            data               = json.loads(request.body)
+            name               = data['name']
+            email              = data['email']
+            password           = data['password']
+            phone_number       = data['phone_number']
+            address            = data['address']
+            result             = User.objects.filter(email=email)
+            regex_email        = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$"
+            regex_password     = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$"
+            regex_phone_number = "^\d{3}-\d{3,4}-\d{4}$"
 
-        if email == "" or password == "":
+            if not re.match(regex_email, email):
+                return JsonResponse({"message": "EMAIL_FORM_ERROR"}, status=400)
+            if not re.match(regex_password, password):
+                return JsonResponse({"message": "PASSWORD_FORM_ERROR"}, status=400)
+            if not re.match(regex_phone_number, phone_number):
+                return JsonResponse({"message": "PHONE_NUMBER_FORM_ERROR"}, status=400)
+
+            if not result:
+                User.objects.create(
+                    name         = name,
+                    email        = email,
+                    password     = password,
+                    phone_number = phone_number,
+                    address      = address
+                    )
+                return JsonResponse({"message" : "SUCCESS"}, status=201)
+            return JsonResponse({"message": "ALREADY_USED_EMAIL"}, status=400)
+
+        except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)
-        if not email_validation.match(email):
-            return JsonResponse({"message": "EMAIL_FORM_ERROR"}, status=400)
-        if not password_validation.match(password):
-            return JsonResponse({"message": "PASSWORD_FORM_ERROR"}, status=400)
-        if not phone_number_validation.match(phone_number):
-            return JsonResponse({"message": "PHONE_NUMBER_FORM_ERROR"}, status=400)
-
-        if not result:
-            User.objects.create(
-                name         = name,
-                email        = email,
-                password     = password,
-                phone_number = phone_number,
-                address      = address
-                )
-            return JsonResponse({"message" : "SUCCESS"}, status=201)
-        return JsonResponse({"message": "ALREADY_USED_EMAIL"}, status=400)
