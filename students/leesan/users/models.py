e2b167604e17bfd627139e07dc722cbe61aa9c9d
leesan195159 <sniping1997@naver.com>
2022-03-18 13:37:24 +0900

    modified: create, update 추가 및 migrate 진행

--- students/leesan/users/models.py
+++ students/leesan/users/models.py
@@ -7,7 +7,9 @@ class User(models.Model):
     password     = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=50)
     address      = models.CharField(max_length=50)
-
+    created_at   = models.DateTimeField(auto_now_add=True)
+    updated_at   = models.DateTimeField(auto_now=True)
+    
     class Meta:
         db_table = 'users'
 
