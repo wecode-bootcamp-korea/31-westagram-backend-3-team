cbf12d0cf5cbb87507dbfd64ab8a1a1bb1c178e2
leesan195159 <sniping1997@naver.com>
2022-03-18 13:30:08 +0900

    중간저장

--- students/leesan/users/models.py
+++ students/leesan/users/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class User(models.Model):
     name         = models.CharField(max_length=50)
     email        = models.EmailField(max_length=50, unique=True)
-    password     = models.CharField(max_length=50)
+    password     = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=50)
-
+    address      = models.CharField(max_length=50)
     class Meta:
         db_table = 'users'
 
--- students/leesan/users/views.py
+++ students/leesan/users/views.py
@@ -15,6 +15,8 @@ class SignIn(View):
             password                = data['password']
             password_validation     = re.compile("^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$")
             phone_number            = data['phone_number']
+            phone_number_validation = re.compile('/^\d{3}-\d{3,4}-\d{4}$/')
+            address                 = data['address']
             
             if email=="" or password =="":
                 return JsonResponse({"message": "KET_ERROR"}, status=400)
@@ -22,14 +24,17 @@ class SignIn(View):
                 return JsonResponse({"message": "Email을 형식에 맞게 입력하세요"}, status=400)
             if not password_validation.match(password):
                 return JsonResponse({"message": "Password를 형식에 맞게 입력하세요"}, status=400)
-                
+            if not phone_number_validation.match(phone_number):
+                return JsonResponse({"message": "Phone_number를 형식에 맞게 입력하세요"}, status=400)
+
             result = User.objects.filter(email=email)  
             if not result:
                 User.objects.create(
                     name         = name,
                     email        = email,
                     password     = password,
-                    phone_number = phone_number,    
+                    phone_number = phone_number,
+                    address      = address
                     )
                 return JsonResponse({"message" : "SUCCESS"}, status=201)
             return JsonResponse({"message": "다른 사용자가 사용하는 email입니다. 다시 입력해주세요."}, status=400)
@@ -44,7 +49,7 @@ class LogIn(View):
         result_email = User.objects.filter(email=email).exists()
         result_password = User.objects.filter(password=password).exists()
         email_email = User.objects.get(email=email)
-        email_id = email_email.id
+        email_id = email_email.id 
         password__password = User.objects.get(password=password)
         password_id = password__password.id
         if email == "" or password == "":
