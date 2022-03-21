1f385e6deb2a74efbdbd7d9ce2e2638f750839e2
재홍 <jaehong@jaehongs-MacBook-Air.local>
2022-03-21 15:00:43 +0900

    [MODIFY] : 수정사항 반영-2

--- students/jaehong/users/models.py
+++ students/jaehong/users/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
-class User(models.Model) :
-    name         = models.CharField(max_length =100), 
-    email        = models.EmailField(unique = True), 
-    password     = models.CharField(max_length=300), 
-    phone_number = models.CharField(max_length=11, unique= True) 
+class User(models.Model):
+    name = models.CharField(max_length=11)
+    email = models.EmailField(max_length=45, unique=True)
+    password = models.CharField(max_length=100)    
+    phone_number = models.CharField(max_length=11) 
+   
 
-class Meta :
-    db_table = "users"
-# Create your models here.
+    class Meta :
+        db_table = "users"
+
--- students/jaehong/westagram/settings.py
+++ students/jaehong/westagram/settings.py
@@ -12,11 +12,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from my_settings import DATABASES, SECRET_KEY
-import pymysql
+# import pymysql
     
-pymysql.install_as_MySQLdb()
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# pymysql.install_as_MySQLdb()
+# # Build paths inside the project like this: BASE_DIR / 'subdir'.
+# BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'users'
+    'users',
 ]
 
 MIDDLEWARE = [
