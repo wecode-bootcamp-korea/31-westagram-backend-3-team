fe7a25648532f64bddfc374360cea70ca4c0bd54
gleehave <gleehave@gmail.com>
2022-03-21 14:48:14 +0900

    [이강호][refactor] validator.py return 코드 간소화

--- students/gangho/users/validators.py
+++ students/gangho/users/validators.py
@@ -1,9 +1,7 @@
 import re
 
 def validate_email(email):
-    email_expression = re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
-    return email_expression
+    return re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)
 
 def validate_password(password):
-    password_expression = re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$', password)
-    return password_expression
+    return re.match('^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$', password)
