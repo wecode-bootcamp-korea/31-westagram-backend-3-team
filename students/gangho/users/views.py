3ceb090c31a847bb13280f5f1d44f2bf52236cdb
gleehave <gleehave@gmail.com>
2022-03-22 17:20:25 +0900

    [이강호][add] 패스워드 암호화 완료

--- students/gangho/users/views.py
+++ students/gangho/users/views.py
@@ -1,4 +1,6 @@
 import json
+import bcrypt
+import jwt
 
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
@@ -7,16 +9,17 @@ from django.db import IntegrityError
 
 from users.models import User
 from users.validators import validate_email, validate_password
-
+from my_settings import SECRET_KEY, ALGORITHM
 
 class SignupView(View):
     def post(self, request):
         try:
             data = json.loads(request.body)
-            name         = data['name']
-            email        = data['email']
-            password     = data['password']
-            phone_number = data['phone_number']
+            name            = data['name']
+            email           = data['email']
+            password        = data['password']
+            hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            phone_number    = data['phone_number']
 
             validate_email(email)
             validate_password(password)
@@ -29,7 +32,7 @@ class SignupView(View):
             User.objects.create(
                 name            = name,
                 email           = email,
-                password        = password,
+                password        = hashed_password,
                 phone_number    = phone_number
             )
 
@@ -64,7 +67,13 @@ class SigninView(View):
             if not User.objects.filter(email=email, password=password).exists():
                 return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
-            return JsonResponse({'message': 'SUCCESS'}, status=200)
+            user_in_database = User.objects.get(email=email)
+            if not bcrypt.checkpw(password.encode('utf-8'), user_in_database.password.encode('utf-8')):
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+            return JsonResponse({
+                'message': "SUCCESS"
+            }, status=200)
 
         except KeyError:
             return JsonResponse({'message': 'KEY_ERROR'}, status=400)
