0de9c376c21c7bbe6e07bf317aa79debb0a93b78
재홍 <jaehong@jaehongs-MacBook-Air.local>
2022-03-22 15:23:48 +0900

    [MODIFY]: 수정사항 반영

--- students/jaehong/users/views.py
+++ students/jaehong/users/views.py
@@ -29,6 +29,8 @@ class SignUpView (View):
                return JsonResponse({"message" : "INVALID PASSWORD"}, status=400)
            
            
+           
+           
             User.objects.create(
                 
                 name         = name,
@@ -47,17 +49,13 @@ class SignInView(View) :
     def post (self, request) :
         try :
             data     = json.loads(request.body)
-            id       = data["email"]
+            email    = data["email"]
             password = data["password"]
             
-            check_id       = User.objects.filter(email = id).exists()
-            check_password = User.objects.get(email = id)
-
-            if check_id and check_password.password == password:
-                return JsonResponse({"message": "SUCCESS"}, status=200)
-                
-            else : 
+            if not User.objects.filter(email = email, password = password).exists() :
                 return JsonResponse({"message": "INVALID_USER"}, status=401)
+            
+            return JsonResponse({"message": "SUCCESS"}, status=200)
      
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400 )
