78093b5d2c54e66311934f3f8469275ba8877460
gleehave <gleehave@gmail.com>
2022-03-22 13:20:35 +0900

    [이강호][modify] FAIL에 해당하는 코드로 대체함.

--- students/gangho/users/views.py
+++ students/gangho/users/views.py
@@ -61,10 +61,10 @@ class SigninView(View):
             validate_email(email)
             validate_password(password)
 
-            if User.objects.filter(email=email, password=password).exists():
-                return JsonResponse({'message': 'SUCCESS'}, status=200)
+            if not User.objects.filter(email=email, password=password).exists():
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
-            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+            return JsonResponse({'message': 'SUCCESS'}, status=200)
 
         except KeyError:
             return JsonResponse({ 'message': 'KEY_ERROR'}, status=400)
