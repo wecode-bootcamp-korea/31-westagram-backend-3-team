0a609221b04096136b523b4e749af3adcacc536d
leesan195159 <sniping1997@naver.com>
2022-03-22 14:58:45 +0900

    중간저장

--- students/leesan/users/urls.py
+++ students/leesan/users/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/signin', SignIn.as_view()),
-    path('/login', LogIn.as_view()),
+    path('/signin', SignUpView.as_view()),
+    path('/login', LogInView.as_view()),
 ]
 
 
