11653e614343dd31c6f5d2a3bd3832e382c4878e
gleehave <gleehave@gmail.com>
2022-03-22 10:39:13 +0900

    [이강호][refactor] JsonResponse 한줄정리

--- students/gangho/users/views.py
+++ students/gangho/users/views.py
@@ -67,10 +67,6 @@ class SigninView(View):
             return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
         except KeyError:
-            return JsonResponse({
-                'message': 'KEY_ERROR'
-            }, status=400)
+            return JsonResponse({ 'message': 'KEY_ERROR'}, status=400)
         except ValidationError as e:
-            return JsonResponse({
-                'message': e.messages
-            }, status=400)
+            return JsonResponse({ 'message': e.messages}, status=400)
