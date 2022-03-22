c6159e08f4e34287024eebc123c40ddc39dda524
leesan195159 <sniping1997@naver.com>
2022-03-22 16:02:12 +0900

    중간저장

--- students/leesan/users/views.py
+++ students/leesan/users/views.py
@@ -1,5 +1,6 @@
 import json
 import re
+import bcrypt
 
 from django.views import View
 from django.http import JsonResponse
@@ -9,16 +10,18 @@ from .models import User
 class SignUpView(View):
     def post(self, request):
         try:
-            data               = json.loads(request.body)
-            name               = data['name']
-            email              = data['email']
-            password           = data['password']
-            phone_number       = data['phone_number']
-            address            = data['address']
-            result             = User.objects.filter(email=email)
-            regex_email        = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$"
-            regex_password     = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$"
-            regex_phone_number = "^\d{3}-\d{3,4}-\d{4}$"
+            data                    = json.loads(request.body)
+            name                    = data['name']
+            email                   = data['email']
+            password                = data['password']
+            phone_number            = data['phone_number']
+            address                 = data['address']
+            encoded_password        = password.encode('utf-8')
+            hash_password           = bcrypt.hashpw(encoded_password, bcrypt.gensalt())
+            decoded_hashed_password = hash_password.decode('utf-8')
+            regex_email             = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$"
+            regex_password          = "^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+?&]{8,}$"
+            regex_phone_number      = "^\d{3}-\d{3,4}-\d{4}$"
 
             if not re.match(regex_email, email):
                 return JsonResponse({"message": "EMAIL_FORM_ERROR"}, status=400)
@@ -26,25 +29,13 @@ class SignUpView(View):
                 return JsonResponse({"message": "PASSWORD_FORM_ERROR"}, status=400)
             if not re.match(regex_phone_number, phone_number):
                 return JsonResponse({"message": "PHONE_NUMBER_FORM_ERROR"}, status=400)
-
-        #     if not result:
-        #         User.objects.create(
-        #             name         = name,
-        #             email        = email,
-        #             password     = password,
-        #             phone_number = phone_number,
-        #             address      = address
-        #             )
-        #         return JsonResponse({"message" : "SUCCESS"}, status=201)
-        #     return JsonResponse({"message": "ALREADY_USED_EMAIL"}, status=400)
-
-
             if User.objects.filter(email=email):
                     return JsonResponse({"message": "EMAIL_ALREADY_EXISTS"}, status=400)
+
             User.objects.create(
                 name         = name,
                 email        = email,
-                password     = password,
+                password     = decoded_hashed_password,
                 phone_number = phone_number,
                 address      = address
             )
